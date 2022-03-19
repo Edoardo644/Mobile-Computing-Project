97c884f109cebd855ba10a121749ccd3602a6522
Umberto28 <umberto.gen28@gmail.com>
2022-03-19 12:36:46 +0100

    collider

---
+++ Mobile App/Assets/Scripts/Player/PlayerMoving.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoving : MonoBehaviour
+{
+    private Rigidbody2D body;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

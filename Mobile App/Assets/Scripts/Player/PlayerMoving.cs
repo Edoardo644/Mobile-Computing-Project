c817acae908277afbd076785a83d424d5e8ea00e
Umberto28 <umberto.gen28@gmail.com>
2022-03-21 17:50:16 +0100

    animazioni personaggio

--- Mobile App/Assets/Scripts/Player/PlayerMoving.cs
+++ Mobile App/Assets/Scripts/Player/PlayerMoving.cs
@@ -6,10 +6,15 @@ public class PlayerMoving : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool jump;
 
     private void Awake()
     {
+
+        //get parameters
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,6 +23,7 @@ public class PlayerMoving : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
+        //flip player
         if(horizontalInput > 0.01f)
         {
             transform.localScale = Vector3.one;
@@ -30,6 +36,20 @@ public class PlayerMoving : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             body.velocity = new Vector2(body.velocity.x, speed);
+            jump = true;
+            anim.SetTrigger("jump");
+        }
+
+        //set parameters
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("Jump", jump);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Tilemap")
+        {
+            jump = false;
         }
     }
 }

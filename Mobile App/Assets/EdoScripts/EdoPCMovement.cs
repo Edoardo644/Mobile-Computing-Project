31870cc315589523323fab83ce77b7f732d51f86
Edoardo644 <percyjackson.e4@gmail.com>
2022-03-26 18:11:06 +0100

    jumping,moving and collision fixed

--- Mobile App/Assets/EdoScripts/EdoPCMovement.cs
+++ Mobile App/Assets/EdoScripts/EdoPCMovement.cs
@@ -8,12 +8,28 @@ public class EdoPCMovement : MonoBehaviour
     public Controller2D controller;
     float horizontalMove = 0f;
     public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
 
        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
 
     }
 
@@ -22,7 +38,8 @@ public class EdoPCMovement : MonoBehaviour
     {
 
         //move the character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
 
     }
 }

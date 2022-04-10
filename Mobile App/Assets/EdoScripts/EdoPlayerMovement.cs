31e5672b84c8ee16d93ea7019be23520bb22e082
Edoardo644 <percyjackson.e4@gmail.com>
2022-04-10 12:48:54 +0200

    player animation

--- Mobile App/Assets/EdoScripts/EdoPlayerMovement.cs
+++ Mobile App/Assets/EdoScripts/EdoPlayerMovement.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class EdoPlayerMovement : MonoBehaviour
 {
-   private Rigidbody2D body;
-   
-   private void Awake()
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    private Rigidbody2D body;
+    //private Collider2D boxCollider;
+    //private Collider2D circleCollider;
+    private Animator animator;
+    private bool Grounded;
+
+    private void Awake()
     {
+        //Grab references from object
         body = GetComponent<Rigidbody2D>();
+       // boxCollider = GetComponent<BoxCollider2D>();
+        //circleCollider = GetComponent<CircleCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //moving left and right
+        body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
+
+        //flipping left and right while moving
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+       else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1,1,1);
+
+
+        //jumping
+        if (Input.GetKey(KeyCode.UpArrow) && Grounded)
+        {
+            Jump();
+        }
+
+        //rolling ma GetKey andrà modificatoin GetKeyDown per far si che il roll
+        //sia eseguito una volta sola quando premo e non se tengo premuto
+       /* if (Input.GetKey(KeyCode.LeftShift))
+        {
+            boxCollider.enabled = false;
+            circleCollider.enabled = false;
+           
+        }*/
+
+        //Set animator parameters
+        animator.SetBool("Run", horizontalInput != 0);
+        animator.SetBool("Grounded", Grounded);
+
+    }
+
+    private void Jump()
+    {
+        //speed andra' modificato con un altra variabile per sistemare il salto
+
+        body.velocity = new Vector2(body.velocity.x, jumpPower);
+        animator.SetTrigger("Jump");
+        Grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            Grounded = true;
+
     }
 }

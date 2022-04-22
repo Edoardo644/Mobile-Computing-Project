a560ee996ac8d54fee4d7896527c3dd110bca592
Edoardo644 <percyjackson.e4@gmail.com>
2022-04-22 12:02:50 +0200

    spikes

--- Mobile App/Assets/EdoScripts/Health/EdoHealth.cs
+++ Mobile App/Assets/EdoScripts/Health/EdoHealth.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
+using System.Collections;
 
 public class EdoHealth : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
-
+    [Header("Iframes")]
+    //invulnerability
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private float numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
 
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -25,6 +32,7 @@ public class EdoHealth : MonoBehaviour
             //player hurt
             anim.SetTrigger("hurt");
             //iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -45,5 +53,21 @@ public class EdoHealth : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
 
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(6,7,true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 1, 1, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes*2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+
+
+        }
+
+        //invulnerable
+        Physics2D.IgnoreLayerCollision(6, 7, false);
+    }
 
 }

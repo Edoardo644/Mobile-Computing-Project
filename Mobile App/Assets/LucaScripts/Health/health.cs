c34013ef653144b23e0369c5076b2b8f7a52d9ce
luca2310 <luca.catar@gmail.com>
2022-05-06 12:25:36 +0200

    Traps

--- Mobile App/Assets/LucaScripts/Health/health.cs
+++ Mobile App/Assets/LucaScripts/Health/health.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class health : MonoBehaviour
 {
-    [SerializeField] private int startingHealth;
+    [Header ("Health")]
+    [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();    
     }
 
     public void TakeDamage(float _damage)
@@ -23,6 +30,7 @@ public class health : MonoBehaviour
         if(currentHealth > 0)
         {
             anim.SetTrigger("hit");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -41,5 +49,23 @@ public class health : MonoBehaviour
         {
             TakeDamage(1);
         }
+    }
+    
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(6, 9, true);
+        for(int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(6, 9, false);
     }
 }
